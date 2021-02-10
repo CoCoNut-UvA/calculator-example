b4586ffc675e5da8140be2b3702dc12555b5852c
DF <leegbestand@gmail.com>
2021-02-10 17:24:11 +0100

    Update calc to newest version.

--- src/print.c
+++ src/print.c
@@ -4,9 +4,30 @@
 #include "ccngen/enum.h"
 #include "ccn/ccn.h"
 
+/*
+static char *operatorToString(enum binop_type type)
+{
+    switch (type) {
+    case BT_add:
+        return "+";
+    case BT_sub:
+        return "-";
+    }
+
+    return "";
+}
+*/
+
+node_st *PRTprogram(node_st *node)
+{
+    TRAVchildren(node);
+    return node;
+}
+
+
 node_st *PRTnum(node_st *node)
 {
-    printf("%d\n", NUM_VAL(node));
+    printf("%d", NUM_VAL(node));
     return node;
 }
 
